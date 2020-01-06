3be777feee53215c8eecf0fd2f372284bc9a3dc1
nobletrout <nobletrout@gmail.com>
2020-01-06 13:48:35 -0500

    adding cyber yeti

---
+++ cyberyetione.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# vim: ts=2 sts=2 et
+require 'nokogiri'
+require 'net/http'
+require 'net/https'
+require 'json'
+uri = URI('https://hooks.slack.com/services/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')
+
+webcam_noko = Nokogiri::XML(Net::HTTP.get(URI('https://www.cannonmt.com/mountain/webcam-daily-photo')))
+
+picture_url = webcam_noko.xpath('//div[@class="panel-foreground panel-video"]/img').first['src']
+picture_uri = 'https://www.cannonmt.com/' + picture_url
+
+yeti_post = {
+    blocks: [
+        {
+            type: 'image',
+            title: {
+                type: 'plain_text',
+                text: 'Yeti Cave Status'
+            },
+            image_url: picture_uri,
+            alt_text: 'Cannon Cam'
+        }
+    ]
+}
+
+req = Net::HTTP::Post.new(uri, 'Content-type' => 'application/json', 'User-Agent' => 'Curl')
+req.body = yeti_post.to_json
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+res = http.request(req)
+puts res.inspect
+puts yeti_post.to_json
